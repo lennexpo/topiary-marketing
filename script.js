acf50cc34b3aa4861643d24ac6484951d6af930e
lennexpo <lennonnyamajiwa05@gmail.com>
2026-03-28 15:06:59 +0200

    replaced all files

--- script.js
+++ script.js
@@ -16,7 +16,7 @@
   }
 
   window.addEventListener('load', () => setTimeout(hidePreloader, 2000));
-  setTimeout(hidePreloader, 1500); // fallback
+  setTimeout(hidePreloader, 3200); // fallback
 })();
 
 /* ── GSAP HERO ANIMATIONS ─────────────────────── */
